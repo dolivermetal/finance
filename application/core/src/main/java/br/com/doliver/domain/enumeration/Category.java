1b4901d73a145892baf8b8d211eaa798fd685f26
dosilva <dosilva@uolinc.com>
2022-06-06 22:34:09 -0300

    Added some categories

--- application/core/src/main/java/br/com/doliver/domain/enumeration/Category.java
+++ application/core/src/main/java/br/com/doliver/domain/enumeration/Category.java
@@ -1,8 +1,30 @@
 package br.com.doliver.domain.enumeration;
 
+import lombok.Getter;
+
 public enum Category {
-  AUTOMOBILE,
-  BILLS,
-  FOOD,
-  MARKET
+  AUTOMOBILE("Automobile"),
+  BAR("Bar"),
+  BILLS("Bills"),
+  BUY("Buy"),
+  FOOD("Food"),
+  FUEL("Fuel"),
+  HEALTH_CARE("Health Care"),
+  HOME("Home"),
+  INVESTMENT("Investment"),
+  MARKET("Market"),
+  OTHERS("Others"),
+  PERSONAL_CARE("Personal Care"),
+  PET_CARE("Pet Care"),
+  TAXES("Taxes"),
+  TRANSFER("Transfer"),
+  TRANSPORT("Transport"),
+  TRAVEL("Travel");
+
+  @Getter
+  private final String name;
+  Category(final String name) {
+    this.name = name;
+  }
 }
+
